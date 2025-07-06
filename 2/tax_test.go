c62a42b6aab5f7e1e85b6dd700f2ebc82309e3eb
Brando Rocha <brandorocha00@gmail.com>
2025-07-06 15:34:47 -0300

    feat: starting with mocks part 4

--- 2/tax_test.go
+++ 2/tax_test.go
@@ -19,11 +19,16 @@ func TestCalculateTax(t *testing.T) {
 
 func TestCalculateTaxAndSave(t *testing.T) {
 	repository := &TaxRepositoryMock{}
-	repository.On("SaveTax", 10.0).Return(nil)
+	repository.On("SaveTax", 10.0).Return(nil).Once()
 	repository.On("SaveTax", 0.0).Return(errors.New("error saving tax"))
 
 	err := CalculateTaxAndSave(1000.0, repository)
 	assert.Nil(t, err)
+	err = CalculateTaxAndSave(1000.0, repository)
+	assert.Nil(t, err)
+
+	err = CalculateTaxAndSave(0.0, repository)
+	assert.Error(t, err, "error saving tax")
 
 	repository.AssertExpectations(t)
 }
