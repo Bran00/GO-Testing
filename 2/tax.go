bb2f42c0db605fb373866c4a2e3d8c048b6c06f0
Brando Rocha <brandorocha00@gmail.com>
2025-07-06 14:42:00 -0300

    feat: starting with mocks part 1

--- 2/tax.go
+++ 2/tax.go
@@ -3,8 +3,11 @@ package tax
 
 import (
 	"errors"
-	"time"
 )
+
+type Repository interface {
+	SaveTax(amount float64) error
+}
 
 func CalculateTax(amount float64) (float64, error) {
 	if amount <= 0 {
@@ -23,14 +26,22 @@ func CalculateTax(amount float64) (float64, error) {
 }
 
 func CalculateTax2(amount float64) float64 {
-	time.Sleep(time.Millisecond)
-	if amount == 0 {
+	if amount <= 0 {
 		return 0
 	}
 
-	if amount >= 1000 {
+	if amount >= 1000 && amount < 20000 {
 		return 10.0
 	}
 
+	if amount >= 20000 {
+		return 20.0
+	}
+
 	return 5.0
+}
+
+func CalculateTaxAndSave(amount float64, repository Repository) error {
+	tax := CalculateTax2(amount)
+	return repository.SaveTax(tax)
 }
